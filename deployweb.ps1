318b82489eff88dc7e1a419cae984253f341b45c
Daniel Larsen <dalars@microsoft.com>
2020-02-19 08:18:27 +1300

    line break

--- deployweb.ps1
+++ deployweb.ps1
@@ -1,4 +1,5 @@
-az storage blob upload-batch --account-name $env:WEBSTORAGEACCOUNTNAME -d '$web' -s ./webapp/transcriptionviewer/dist/transcriptionviewer
+az storage blob upload-batch --account-name $env:WEBSTORAGEACCOUNTNAME `
+    -d '$web' -s ./webapp/transcriptionviewer/dist/transcriptionviewer
 
 # Get the Web endpoint for the storage account
 $webUrl = ( az storage account show -n $env:WEBSTORAGEACCOUNTNAME | ConvertFrom-Json ).primaryEndpoints.web
