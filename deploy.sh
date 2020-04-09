af4b754ff48188d1b91a5e43b5f1a292bb214104
Tim Leyden <tim.leyden@microsoft.com>
2020-04-09 11:31:13 +1000

    adding double slash for gbash

--- deploy.sh
+++ deploy.sh
@@ -48,7 +48,7 @@ DeployWithoutFuncPack() {
   # 1. Build
   nuget.exe restore "$DEPLOYMENT_SOURCE\src\speechtotextdemo.csproj" -MSBuildPath "$MSBUILD_15_DIR"
   echo "$MSBUILD_15_DIR\MSBuild.exe" "$DEPLOYMENT_SOURCE\src\speechtotextdemo.csproj" /p:DeployOnBuild=true /p:configuration=Release /p:publishurl=$DEPLOYMENT_TEMP $SCM_BUILD_ARGS
-  "$MSBUILD_15_DIR\MSBuild.exe" "$DEPLOYMENT_SOURCE\src\speechtotextdemo.csproj" /p:DeployOnBuild=true /p:configuration=Release /p:publishurl=$DEPLOYMENT_TEMP $SCM_BUILD_ARGS
+  "$MSBUILD_15_DIR\MSBuild.exe" "$DEPLOYMENT_SOURCE\src\speechtotextdemo.csproj" //p:DeployOnBuild=true //p:configuration=Release //p:publishurl=$DEPLOYMENT_TEMP $SCM_BUILD_ARGS
     # 2. KuduSync
     if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
       "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh;obj"
