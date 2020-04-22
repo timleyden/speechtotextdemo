027cb18e2124eebd41238eaf8932d58717688b65
Tim Leyden <tim.leyden@microsoft.com>
2020-04-23 00:40:03 +1000

    excluding non dist files from kudusync

--- deploy.sh
+++ deploy.sh
@@ -268,7 +268,7 @@ fi
 
 # 1. KuduSync
 if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
-  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE/webapp/transcriptionviewer" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
+  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE/webapp/transcriptionviewer/dist" -t "$DEPLOYMENT_TARGET/dist" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
   exitWithMessageOnError "Kudu Sync failed"
 fi
 
