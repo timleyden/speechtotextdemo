5d9b8ab1f7e9aeddd18864b6906945a374dc14fa
Tim Leyden <tim.leyden@microsoft.com>
2020-04-28 09:08:37 +1000

    move speech config to function proxy, remove info from client config and update node js to inject app settings rather than replace at build time

--- webapp/transcriptionviewer/webserver/app.js
+++ webapp/transcriptionviewer/webserver/app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 8080;
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-var router = express.Router();
 
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+var fs = require('fs'),
+    path = require('path'),
+    filePath = path.join(__dirname, '/dist/transcriptionviewer/assets/config/config.prod.json');
 console.log('——————————- Run on port '+ port);
 
 /****************************** Router ***************************/
@@ -15,13 +17,23 @@ router.get('*', function(req, res){
 
 /****************************** /Router ***************************/
 
-//app.use(morgan('dev')); // log every request to the console
-app.use(express.static(__dirname + '/dist/transcriptionviewer')); // Static (public) folder
 
-app.use(bodyParser.urlencoded({extended:true}));// get information from html forms
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(methodOverride());
+app.get('/assets/config/config.prod.json',function(req,res){
+  console.log('got here')
+  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (!err) {
+        console.log('received data: ' + data);
+        var result = data.replace(/#{appInsightsKey}/g, process.env.APPSETTING_APPINSIGHTS_INSTRUMENTATIONKEY);
+       result = result.replace(/#{configUrl}/g, process.env.APPSETTING_configServiceUrl);
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.write(result);
+        res.end();
+    } else {
+        console.log(err);
+    }
+});
+});
+app.use(express.static(__dirname + '/dist/transcriptionviewer')); // Static (public) folder
 app.use('/', router); // app.use('/parent', router); call all from localhost:port/parent/*
 
 app.listen(port);
